bde6b6078fd85d8a34d351324a9e5e7fac2a7267
Dheeraj Reddy <dheeraj.reddy@intel.com>
2022-05-23 16:03:01 -0700

    Fix the awk script by closing the file descriptors getline fails when we have too many open files (large cpu count machines) Update print format.

--- cpuinfo.sh
+++ cpuinfo.sh
@@ -23,29 +23,50 @@ BEGIN {
 }
 
 END {
-    printf("CPU\tPkg\tCore\tAPICi\tAPIC\tCache0\tCache1\tCache2\tCache3\n");
+    printf("CPU\tPkg\tCore\tAPICi\tAPIC\tCache0(sharing)\tCache1(sharing)\tCache2(sharing)\tCache3(sharing)\n");
     for (i=0; i<cpu_count; i++) {
 	cpuidx=cpu_number[i]
 
 	fname = cpupath cpuidx "/cache/index0/size"
 	getline c0size < fname
+	close(fname)
+        fname = cpupath cpuidx "/cache/index0/shared_cpu_list"
+        getline c0share < fname
+	close(fname)
+
 	fname = cpupath cpuidx "/cache/index1/size"
 	getline c1size < fname
+	close(fname)
+        fname = cpupath cpuidx "/cache/index1/shared_cpu_list"
+        getline c1share < fname
+	close(fname)
+
 	fname = cpupath cpuidx "/cache/index2/size"
 	getline c2size < fname
+	close(fname)
+        fname = cpupath cpuidx "/cache/index2/shared_cpu_list"
+        getline c2share < fname
+	close(fname)
+
 	fname = cpupath cpuidx "/cache/index3/size"
 	getline c3size < fname
+	close(fname)
+	fname = cpupath cpuidx "/cache/index3/shared_cpu_list"
+	getline c3share < fname
+	close(fname)
 	
 	fname = cpupath cpuidx "/topology/physical_package_id"
 	getline pkgid < fname
+	close(fname)
 
 	fname = cpupath cpuidx "/topology/core_id"
 	getline coreid < fname
+	close(fname)
 
 
-	printf("%d\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\n", cpuidx, pkgid, coreid,
-		ini_apicid[cpuidx], apicid[cpuidx], c0size, c1size, c2size,
-		c3size);
+	printf("%d\t%s\t%s\t%s\t%s\t%s(%s)\t%s(%s)\t%s(%s)\t%s(%s)\n", cpuidx, pkgid, coreid,
+		ini_apicid[cpuidx], apicid[cpuidx], c0size, c0share, c1size, c1share, c2size, c2share,
+		c3size, c3share);
     }
 }
 '
