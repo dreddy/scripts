c2af1f4c991e414be5d2e3e11581f3521cb12442
Dheeraj Reddy <dheeraj.reddy@intel.com>
2022-12-09 13:53:56 -0800

    Add msrutils to cook up small progs in ramdisk

---
+++ msrutils.h
@@ -0,0 +1,45 @@
+
+#define _GNU_SOURCE
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <assert.h>
+
+/* Caller has to deal with the fd himself */
+int open_msrfd(int cpu)
+{
+	int msr_fd;
+	char msr_fname[64];
+	sprintf(msr_fname, "/dev/cpu/%d/msr", cpu);
+	msr_fd = open(msr_fname, O_RDWR);
+	if (msr_fd < 0) {
+		perror("open: ");
+		exit(-1);
+	}
+	return msr_fd;
+}
+
+int close_msrfd(int fd)
+{
+	close(fd);
+}
+
+int read_msr(int fd, uint64_t reg, uint64_t *val)
+{
+	assert(fd>0);
+	if (pread(fd, val, sizeof(uint64_t), (off_t)reg) != sizeof(uint64_t))
+		perror("rdmsr: ");
+	return 0;
+}
+
+int write_msr(int fd, uint64_t reg, uint64_t val)
+{
+	assert(fd>0);
+	if (pwrite(fd, &val, sizeof val, (off_t)reg) != sizeof val)
+		perror("wrmsr: ");
+	return 0;
+}
