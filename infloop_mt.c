3c4da73197814d6b48077bd53f1e549c02bbe3fb
Dheeraj Reddy <dheeraj.reddy@intel.com>
2023-03-21 11:30:03 -0700

    Fix infloop_mt for pthread_create races

--- infloop_mt.c
+++ infloop_mt.c
@@ -6,6 +6,7 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdio.h>
 
 void* loop (void* arg)
 {
@@ -17,22 +18,26 @@ int main(int ac, char *av[])
 {
         long ncpus;
         pthread_t *tid = malloc (sizeof (pthread_t) * ncpus);
-        int i;
+        int i, rv;
         pthread_attr_t attr;
         cpu_set_t cpuset;
-        
+
         ncpus = (ac==2) ? strtoul(av[1], NULL, 10): sysconf(_SC_NPROCESSORS_ONLN);
         for (i=0; i<ncpus; i++) {
                 CPU_ZERO(&cpuset);
                 CPU_SET(i, &cpuset);
                 pthread_attr_init(&attr);
                 pthread_attr_setaffinity_np(&attr, sizeof (cpuset), &cpuset);
-                tid[i] = pthread_create(&tid[i], &attr, loop, NULL);
+                rv = pthread_create(&tid[i], &attr, loop, NULL);
+		if (rv < 0) {
+                        printf("pthread_create failed\n");
+                        exit(-1);
+		}
         }
-        
+
         for (i=0; i<ncpus; i++)
-                pthread_join(tid[0], NULL);
-        
+                pthread_join(tid[i], NULL);
+
         pthread_attr_destroy(&attr);
         pthread_exit(NULL);
         return 0;
